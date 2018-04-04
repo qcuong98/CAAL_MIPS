396da41cc48c9e13f081bf3c098e3f8128fe46a5
Do Duy <juchiast@gmail.com>
2018-04-04 10:08:50 +0700

    add LeapYear, fix mem uses

--- main.asm
+++ main.asm
@@ -38,9 +38,10 @@ EndMonth:
 
 j EndYear
 Year:
-addiu $sp, $sp, -8
+addiu $sp, $sp, -12
 sw $ra, 0($sp)
 sw $a0, 4($sp)
+sw $s0, 8($sp)
 addiu $a0, $a0, 6
 jal Day
 sll $t0, $v0, 2
@@ -53,10 +54,41 @@ lw $a0, 4($sp)
 addiu $a0, $a0, 8
 jal Day
 add $v0, $s0, $v0
+lw $s0, 8($sp)
 lw $ra, 0($sp)
-addiu $sp, $sp, 8
+addiu $sp, $sp, 12
 jr $ra
 EndYear:
+
+
+j EndLeapYear
+LeapYear:
+addiu $sp, $sp, -4
+sw $ra, 0($sp)
+jal Year
+or $t0, $0, $v0
+ori $t1, $0, 400
+div $t0, $t1
+mfhi $t2
+beq $t2, $0, LeapYear_True
+ori $t1, $0, 100
+div $t0, $t1
+mfhi $t2
+beq $t2, $0, LeapYear_False
+andi $t2, $t0, 3
+bne $t2, $0, LeapYear_False
+j LeapYear_True
+LeapYear_False:
+lw $ra, 0($sp)
+addiu $sp, $sp, 4
+ori $v0, $0, 0
+jr $ra
+LeapYear_True:
+lw $ra, 0($sp)
+addiu $sp, $sp, 4
+ori $v0, $0, 1
+jr $ra
+EndLeapYear:
 
 
 j EndMain
