e71bbd37167d8a885b0a4266739cd220319ed3b7
CUONG TRAN <tranquoccuong@outlook.com.vn>
2018-04-05 20:42:27 +0700

    add comments

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-j Main
+j LeapYear
 
 
 j EndMalloc
@@ -12,11 +12,13 @@ EndMalloc:
 
 j EndDay
 Day:
+# get first digit of a0
 lb $v0, 0($a0)
 addiu $v0, $v0, -48
 sll $t0, $v0, 2
 addu $t0, $t0, $v0
 addu $v0, $t0, $t0
+# get second digit of a0
 lb $t0, 1($a0)
 addiu $t0, $t0, -48
 addu $v0, $v0, $t0
@@ -29,6 +31,7 @@ Month:
 addiu $sp, $sp, -4
 sw $ra, 0($sp)
 addiu $a0, $a0, 3
+# get forth and fifth digit of a0
 jal Day
 lw $ra, 0($sp)
 addiu $sp, $sp, 4
@@ -43,6 +46,7 @@ sw $ra, 0($sp)
 sw $a0, 4($sp)
 sw $s0, 8($sp)
 addiu $a0, $a0, 6
+# get seventh and eighth digit of a0
 jal Day
 sll $t0, $v0, 2
 addu $t0, $t0, $v0
@@ -52,6 +56,7 @@ addu $t0, $t0, $v0
 addu $s0, $t0, $t0
 lw $a0, 4($sp)
 addiu $a0, $a0, 8
+# get ninth and tenth digit of a0
 jal Day
 add $v0, $s0, $v0
 lw $s0, 8($sp)
@@ -66,15 +71,19 @@ LeapYear:
 addiu $sp, $sp, -4
 sw $ra, 0($sp)
 jal Year
+# t0 = Year
 or $t0, $0, $v0
+# t2 = t0 % 400
 ori $t1, $0, 400
 div $t0, $t1
 mfhi $t2
 beq $t2, $0, LeapYear_True
+# t2 = t0 % 100
 ori $t1, $0, 100
 div $t0, $t1
 mfhi $t2
 beq $t2, $0, LeapYear_False
+# t2 = t0 % 4
 andi $t2, $t0, 3
 bne $t2, $0, LeapYear_False
 j LeapYear_True
